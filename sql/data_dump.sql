1887c7f41bbe3356c05faaf7263e9d0b0fe8ffad
thomasmulhall410 <thomasmulhall410@yahoo.com>
2019-04-09 21:53:24 +0000

    Add date dump was generated at
    Bug: T4258
    Change-Id: I24799ef92bb7c9b569d7551fbf4858abc5e67ac6

--- sql/data_dump.sql
+++ sql/data_dump.sql
@@ -2,5 +2,6 @@ CREATE TABLE /*_*/data_dump (
   `dumps_completed` INT(1) NOT NULL,
   `dumps_filename` LONGTEXT NOT NULL,
   `dumps_failed` INT(1) NOT NULL,
+  `dumps_timestamp` VARCHAR(14) NOT NULL,
   `dumps_type` LONGTEXT NOT NULL
 ) /*$wgDBTableOptions*/;
