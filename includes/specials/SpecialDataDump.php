bcb09a78d33ff2d6ac82a8e73d20c81866f160e7
paladox <paladox@users.noreply.github.com>
2019-12-22 17:04:02 +0000

    Replace deprecated/removed getTitle function with getPageTitle

--- includes/specials/SpecialDataDump.php
+++ includes/specials/SpecialDataDump.php
@@ -124,7 +124,7 @@ class SpecialDataDump extends SpecialPage {
 
 				$logEntry = new ManualLogEntry( 'datadump', 'generate' );
 				$logEntry->setPerformer( $this->getUser() );
-				$logEntry->setTarget( $this->getTitle() );
+				$logEntry->setTarget( $this->getPageTitle() );
 				$logEntry->setComment( 'Generated dump' );
 				$logEntry->setParameters( [ '4::filename' => $fileName ] );
 				$logEntry->publish( $logEntry->insert() );
@@ -281,7 +281,7 @@ class SpecialDataDump extends SpecialPage {
 
 		$logEntry = new ManualLogEntry( 'datadump', 'delete' );
 		$logEntry->setPerformer( $this->getUser() );
-		$logEntry->setTarget( $this->getTitle() );
+		$logEntry->setTarget( $this->getPageTitle() );
 		$logEntry->setComment( 'Deleted dumps' );
 		$logEntry->setParameters( [ '4::filename' => implode( ', ', $fileNames ) ] );
 		$logEntry->publish( $logEntry->insert() );
