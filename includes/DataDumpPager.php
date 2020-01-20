67a34dd5bf2e86b72282334f13dc6cfc9dfe4712
paladox <paladox@users.noreply.github.com>
2020-01-20 23:23:47 +0000

    Create DataDumpPager.php

---
+++ includes/DataDumpPager.php
@@ -0,0 +1,98 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+class DataDumpPager extends TablePager {
+	function __construct() {
+		$this->mDb = wfGetDB( DB_MASTER );
+
+		if ( $this->getRequest()->getText( 'sort', 'files_date' ) == 'dumps_date' ) {
+			$this->mDefaultDirection = IndexPager::DIR_DESCENDING;
+		} else {
+			$this->mDefaultDirection = IndexPager::DIR_ASCENDING;
+		}
+
+		parent::__construct( $this->getContext() );
+        }
+
+	function getFieldNames() {
+		static $headers = null;
+
+		$headers = [
+			'dumps_timestamp' => 'listfiles_date',
+			'dumps_filename'    => 'name',
+			'dumps_type'      => 'type',
+			'dumps_ready'       => 'ready',
+                        'dumps_delete'      => 'delete',
+		];
+
+		foreach ( $headers as &$msg ) {
+			$msg = $this->msg( $msg )->text();
+		}
+
+		return $headers;
+	}
+
+	function formatValue( $name, $value ) {
+		$row = $this->mCurrentRow;
+
+		switch ( $name ) {
+			case 'dumps_timestamp':
+                                $time = isset( $row->dumps_timestamp ) ? $row->dumps_timestamp : '';
+				$formatted = htmlspecialchars( $this->getLanguage()->userTimeAndDate( $time, $this->getUser() ) );
+				break;
+			case 'dumps_filename';
+				$url = SpecialPage::getTitleFor( 'DataDump' )->getFullUrl() .
+						'/download/' . $row->dumps_filename;
+				$formatted = Linker::makeExternalLink( $url, $row->dumps_filename );
+				break;
+			case 'dumps_type':
+				$formatted = $row->dumps_type;
+				break;
+			case 'dumps_ready':
+                                if ($row->dumps_completed == 1) {
+                                  $formatted = "Ready";
+                                } else if ( $row->dumps_failed == 1 ) {
+                                  $formatted = "Failed";
+                                } else {
+				  $formatted = "Queued";
+                                }
+				break;
+                        case 'dumps_delete':
+                                $url = SpecialPage::getTitleFor( 'DataDump' )->getFullUrl() .
+                                                 '/delete/' . $row->dumps_filename;
+                                $formatted = Linker::makeExternalLink( $url, 'Delete' );
+                                break;
+			default:
+				$formatted = "Unable to format $name";
+				break;
+		}
+
+		return $formatted;
+	}
+
+	function getQueryInfo() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'mirahezemagic' );
+
+		$info = [
+			'tables' => [ 'data_dump' ],
+			'fields' => [ 'dumps_completed', 'dumps_filename', 'dumps_failed', 'dumps_timestamp', 'dumps_type' ],
+			'conds' => [],
+			'joins_conds' => [],
+		];
+
+		if ( !$config->get( 'User' )->isAllowed( 'viewglobalprivatefiles' ) ) {
+		//	$info['conds']['files_private'] = 0;
+		}
+
+		return $info;
+	}
+
+	function getDefaultSort() {
+		return 'dumps_timestamp';
+	}
+
+	function isFieldSortable( $name ) {
+		return true;
+	}
+}
