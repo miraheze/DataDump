d898d71fc9b7be83c9cf394530f66f842c61a643
paladox <paladox@users.noreply.github.com>
2020-03-25 23:04:15 +0000

    Update DataDumpHooks.php

--- includes/DataDumpHooks.php
+++ includes/DataDumpHooks.php
@@ -25,4 +25,14 @@ class DataDumpHooks {
 			__DIR__ . '/../sql/patches/patch-dumps_size.sql'
 		);
 	}
+
+	public static function onNewSidebarItem( $skin, &$bar ) {
+		if ( isset( $bar['Administration'] ) && $bar['Administration'] ) {
+			$bar['Administration'][] = [
+				'text' => wfMessage( "datadump-link" )->plain(),
+				'id' => "datadumplink",
+				'href' => htmlspecialchars( SpecialPage::getTitleFor( 'DataDump' )->getFullURL() )
+			];
+		}
+	}
 }
