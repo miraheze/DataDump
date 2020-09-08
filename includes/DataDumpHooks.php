805527b70f94decaf07247c54ffef6c649e5042c
paladox <paladox@users.noreply.github.com>
2020-09-08 23:00:53 +0100

    Update DataDumpHooks.php

--- includes/DataDumpHooks.php
+++ includes/DataDumpHooks.php
@@ -24,5 +24,8 @@ class DataDumpHooks {
 			'dumps_size',
 			__DIR__ . '/../sql/patches/patch-dumps_size.sql'
 		);
+
+		$updater->modifyExtensionTable( 'mw_namespaces',
+				__DIR__ . '/../sql/patches/patch-dumps_size-bigint.sql' );
 	}
 }
