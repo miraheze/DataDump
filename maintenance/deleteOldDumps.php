f619662fe689bd0752d3c2785fdd37eb791c4e08
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-12-31 12:24:19 -0700

    DeleteOldDumps: delete limit by dump type, add dry run, and other fixes

--- maintenance/deleteOldDumps.php
+++ maintenance/deleteOldDumps.php
@@ -14,34 +14,71 @@ class DeleteOldDumps extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addDescription( 'Delete the oldest dumps if the dump limit is exceeded' );
+		$this->addOption( 'dry-run', 'Perform a dry run and do not actually delete any dumps' );
+
 		$this->requireExtension( 'DataDump' );
 	}
 
 	public function execute() {
+		$db = wfGetDB( DB_PRIMARY );
+		$dryRun = $this->getOption( 'dry-run', false );
+
+		// Get the dump types and their limits from the config
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'datadump' );
+		$dumpTypes = $config->get( 'DataDump' );
 
-		$db = $this->getDB( DB_PRIMARY );
-		$res = $db->select( 'data_dump', '*', [], __METHOD__, [ 'ORDER BY' => 'dumps_timestamp ASC' ] );
+		// Loop through each dump type
+		foreach ( $dumpTypes as $dumpType => $typeConfig ) {
+			$limit = $typeConfig['limit'];
 
-		$backend = DataDump::getBackend();
-		$storagePath = $backend->getContainerStoragePath( 'dumps-backup' );
-
-		$dumpCount = 0;
-		foreach ( $res as $row ) {
-			$dumpType = $row->dumps_type;
-			$dumpLimit = $config->get( 'DataDump' )[$dumpType]['limit'];
-
-			if ( $dumpLimit < 0 ) {
+			// If the limit is 0 or less, there is no limit for this dump type
+			if ( $limit <= 0 ) {
 				continue;
 			}
 
-			$dumpCount++;
-			if ( $dumpCount > $dumpLimit ) {
-				# Delete the dump from the data_dump table
-				$db->delete( 'data_dump', [ 'dumps_filename' => $row->dumps_filename ], __METHOD__ );
+			// Get the current number of dumps of this type
+			$numDumps = (int)$db->selectField(
+				'data_dump',
+				'COUNT(*)',
+				[ 'dumps_type' => $dumpType ],
+				__METHOD__
+			);
 
-				# Delete the dump file from storage
-				$backend->delete( [ 'src' => "$storagePath/{$row->dumps_filename}" ] );
+			// If the number of dumps is already at or below the limit, there is nothing to do
+			if ( $numDumps <= $limit ) {
+				continue;
+			}
+
+			// Get the oldest dump of this type
+			$oldestDump = $db->selectRow(
+				'data_dump',
+				[ 'dumps_filename' ],
+				[ 'dumps_type' => $dumpType ],
+				__METHOD__,
+				[ 'ORDER BY' => 'dumps_timestamp ASC' ]
+			);
+
+			// If there is no oldest dump, there is nothing to do
+			if ( !$oldestDump ) {
+				continue;
+			}
+
+			$oldestFilename = $oldestDump->dumps_filename;
+
+			// Delete the oldest dump
+			if ( !$dryRun ) {
+				$backend = DataDump::getBackend();
+				$storagePath = $backend->getContainerStoragePath( 'dumps-backup' );
+				$backend->delete( [ 'src' => "$storagePath/$oldestFilename" ] );
+
+				// Delete the dump from the data_dump table
+				$db->delete(
+					'data_dump',
+					[ 'dumps_filename' => $oldestFilename ],
+					__METHOD__
+				);
+			} else {
+				$this->output( "Would delete dump $oldestFilename\n" );
 			}
 		}
 	}
