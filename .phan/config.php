6a55e59cb54e6d7d5b38df56171cf7aa3ad0e7db
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-26 13:57:55 -0700

    Fix phan for 1.41

--- .phan/config.php
+++ .phan/config.php
@@ -3,9 +3,8 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['suppress_issue_types'] = [
+	'PhanAccessMethodInternal',
 	'SecurityCheck-LikelyFalsePositive',
-	// Different versions of MediaWiki will need different suppressions.
-	'UnusedPluginSuppression'
 ];
 
 return $cfg;
