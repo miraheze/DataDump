bfce3b67182df24fb6c837b8eda04ef69af59f6a
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-29 12:24:59 -0700

    Update

--- includes/Hooks/Handlers/Installer.php
+++ includes/Hooks/Handlers/Installer.php
@@ -12,41 +12,41 @@ class Installer implements LoadExtensionSchemaUpdatesHook {
 	 * @param DatabaseUpdater $updater
 	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$dir = __DIR__ . '/../../../sql';
+		$dir = __DIR__ . '/../../..';
 
-		$updater->addExtensionTable( 'data_dump', "$dir/data_dump.sql" );
+		$updater->addExtensionTable( 'data_dump', "$dir/sql/data_dump.sql" );
 
 		$updater->addExtensionField(
 			'data_dump',
 			'dumps_timestamp',
-			"$dir/patches/patch-dumps_timestamp.sql"
+			"$dir/sql/patches/patch-dumps_timestamp.sql"
 		);
 
 		$updater->addExtensionField(
 			'data_dump', 'dumps_size',
-			"$dir/patches/patch-dumps_size.sql"
+			"$dir/sql/patches/patch-dumps_size.sql"
 		);
 
-		$updater->modifyExtensionTable( 'data_dump', "$dir/patches/patch-dumps_size-bigint.sql" );
+		$updater->modifyExtensionTable( 'data_dump', "$dir/sql/patches/patch-dumps_size-bigint.sql" );
 
 		$updater->addExtensionField(
 			'data_dump', 'dumps_status',
-			"$dir/patches/patch-dumps_status.sql"
+			"$dir/sql/patches/patch-dumps_status.sql"
 		);
 
 		$updater->addExtensionUpdate( [
 			'runMaintenance', MigrateCompletedAndFailedToStatusColumn::class,
-			'extensions/DataDump/maintenance/migrateCompletedAndFailedToStatusColumn.php'
+			"$dir/maintenance/migrateCompletedAndFailedToStatusColumn.php"
 		] );
 
 		$updater->dropExtensionField(
 			'data_dump', 'dumps_completed',
-			"$dir/patches/patch-drop-dumps_completed.sql"
+			"$dir/sql/patches/patch-drop-dumps_completed.sql"
 		);
 
 		$updater->dropExtensionField(
 			'data_dump', 'dumps_failed',
-			"$dir/patches/patch-drop-dumps_failed.sql"
+			"$dir/sql/patches/patch-drop-dumps_failed.sql"
 		);
 	}
 }
