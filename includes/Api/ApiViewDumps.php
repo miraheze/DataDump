ec4ddec5241cccf96159a9a5cf8e5f7022087ee7
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-17 12:47:47 -0700

    Cleanup

--- includes/Api/ApiViewDumps.php
+++ includes/Api/ApiViewDumps.php
@@ -5,13 +5,13 @@ namespace Miraheze\DataDump\Api;
 use ApiBase;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\SpecialPage\SpecialPage;
+use stdClass;
 use Wikimedia\ParamValidator\ParamValidator;
 
 class ApiViewDumps extends ApiBase {
-	public function execute() {
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'DataDump' );
-		$dataDumpConfig = $config->get( 'DataDump' );
 
+	public function execute(): void {
+		$dataDumpConfig = $this->getConfig()->get( 'DataDump' );
 		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
 
 		if ( !$dataDumpConfig ) {
@@ -41,53 +41,56 @@ class ApiViewDumps extends ApiBase {
 			$buildWhichArray['dumps_timestamp'] = $params['timestamp'];
 		}
 
-		$dumpData = MediaWikiServices::getInstance()
-			->getDBLoadBalancer()
-			->getMaintenanceConnectionRef( DB_PRIMARY )->select(
+		$res = MediaWikiServices::getInstance()
+			->getConnectionProvider()
+			->getReplicaDatabase()->select(
 				'data_dump',
 				'*',
-				$buildWhichArray
+				$buildWhichArray,
+				__METHOD__
 			);
 
 		$buildResults = [];
-		if ( $dumpData ) {
+		if ( $res ) {
 			$user = $this->getUser();
-			foreach ( $dumpData as $dump ) {
-				$perm = $dataDumpConfig[$dump->dumps_type]['permissions']['view'] ?? 'view-dump';
+			foreach ( $res as $row ) {
+				$perm = $dataDumpConfig[$row->dumps_type]['permissions']['view'] ?? 'view-dump';
 
 				if ( !$permissionManager->userHasRight( $user, $perm ) ) {
 					continue;
 				}
 
 				$buildResults[] = [
-					'filename' => $dump->dumps_filename,
-					'link' => $this->getDownloadUrl( $config, $dump ),
-					'time' => $dump->dumps_timestamp ?: '',
-					'type' => $dump->dumps_type,
-					'status' => $dump->dumps_status,
+					'filename' => $row->dumps_filename,
+					'link' => $this->getDownloadUrl( $row ),
+					'time' => $row->dumps_timestamp ?: '',
+					'type' => $row->dumps_type,
+					'status' => $row->dumps_status,
 				];
 			}
 		}
+
 		$this->getResult()->addValue( null, $this->getModuleName(), $buildResults );
 	}
 
-	private function getDownloadUrl( $config, $dump ) {
+	private function getDownloadUrl( stdClass $row ): string {
 		// Do not create a link if the file has not been created.
-		if ( (int)$dump->dumps_completed !== 1 ) {
-			return $dump->dumps_filename;
+		if ( (int)$row->dumps_completed !== 1 ) {
+			return $row->dumps_filename;
 		}
 
 		$title = SpecialPage::getTitleFor( 'DataDump' );
 
 		$query = [
 			'action' => 'download',
-			'dump' => $dump->dumps_filename,
+			'dump' => $row->dumps_filename,
 		];
 
 		return $title->getFullURL( $query );
 	}
 
-	public function getAllowedParams() {
+	/** @inheritDoc */
+	public function getAllowedParams(): array {
 		return [
 			'type' => [
 				ParamValidator::PARAM_TYPE => 'string',
@@ -102,7 +105,7 @@ class ApiViewDumps extends ApiBase {
 	}
 
 	/** @inheritDoc */
-	protected function getExamplesMessages() {
+	protected function getExamplesMessages(): array {
 		return [
 			'action=viewdumps'
 				=> 'apihelp-viewdumps-example-1',
