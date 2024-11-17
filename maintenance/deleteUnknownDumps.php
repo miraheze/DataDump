05b1648721b036e94b1d8aab32c3ea3c585d4631
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-17 13:43:14 -0700

    Delete chunks

--- maintenance/deleteUnknownDumps.php
+++ maintenance/deleteUnknownDumps.php
@@ -13,6 +13,7 @@ use Maintenance;
 use Miraheze\DataDump\DataDump;
 
 class DeleteUnknownDumps extends Maintenance {
+
 	public function __construct() {
 		parent::__construct();
 
@@ -22,32 +23,51 @@ class DeleteUnknownDumps extends Maintenance {
 		$this->requireExtension( 'DataDump' );
 	}
 
-	public function execute() {
+	public function execute(): void {
 		$dumpTypes = array_keys( $this->getConfig()->get( 'DataDump' ) );
 		$dryRun = $this->getOption( 'dry-run', false );
 
 		$db = $this->getDB( DB_PRIMARY );
 		$res = $db->select( 'data_dump', '*', [], __METHOD__ );
 
-		$backend = DataDump::getBackend();
-		$storagePath = $backend->getContainerStoragePath( 'dumps-backup' );
-
 		foreach ( $res as $row ) {
 			if ( !in_array( $row->dumps_type, $dumpTypes ) ) {
 				if ( !$dryRun ) {
-					# Delete the dump from the data_dump table
-					$db->delete( 'data_dump', [ 'dumps_filename' => $row->dumps_filename ], __METHOD__ );
+					// Delete the dump file from storage
+					$this->deleteFileChunks( $row->dumps_filename );
 
-					# Delete the dump file from storage
-					$backend->delete( [ 'src' => "$storagePath/{$row->dumps_filename}" ] );
+					// Delete the dump from the data_dump table
+					$db->delete( 'data_dump', [ 'dumps_filename' => $row->dumps_filename ], __METHOD__ );
 				} else {
-					# Output what would be deleted if this was not a dry run
+					// Output what would be deleted if this was not a dry run
 					$this->output( "Would delete dump with filename {$row->dumps_filename} and type {$row->dumps_type}\n" );
 				}
 			}
 		}
 
 		$this->output( "Done\n" );
+	}
+
+	private function deleteFileChunks( string $fileName ): void {
+		$backend = DataDump::getBackend();
+		$fileBackend = $backend->getContainerStoragePath( 'dumps-backup' ) . '/' . $fileName;
+		$chunkIndex = 0;
+
+		while ( $backend->fileExists( [ 'src' => $fileBackend . '.part' . $chunkIndex ] ) ) {
+			$chunkFileBackend = $fileBackend . '.part' . $chunkIndex;
+			$delete = $backend->delete( [ 'src' => $chunkFileBackend ] );
+			if ( !$delete->isOK() ) {
+				$this->fatalError( 'Failed to delete ' . $chunkFileBackend );
+			}
+			$chunkIndex++;
+		}
+
+		if ( $backend->fileExists( [ 'src' => $fileBackend ] ) ) {
+			$delete = $backend->delete( [ 'src' => $fileBackend ] );
+			if ( !$delete->isOK() ) {
+				$this->fatalError( 'Failed to delete ' . $fileBackend );
+			}
+		}
 	}
 }
 
