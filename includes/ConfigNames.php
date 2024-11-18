6df21eaddb0ab3c1e5ac1cad7b78882c7e605849
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-17 18:16:06 -0700

    Add ConfigNames

---
+++ includes/ConfigNames.php
@@ -0,0 +1,21 @@
+<?php
+
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName.ClassConstantNotUpperCase
+namespace Miraheze\DataDump;
+
+/**
+ * A class containing constants representing the names of configuration variables,
+ * to protect against typos.
+ */
+class ConfigNames {
+
+	public const DataDump = 'DataDump';
+
+	public const Directory = 'DataDumpDirectory';
+
+	public const DisableGenerate = 'DataDumpDisableGenerate';
+
+	public const FileBackend = 'DataDumpFileBackend';
+
+	public const Limits = 'DataDumpLimits';
+}
